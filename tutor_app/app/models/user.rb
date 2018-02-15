51d9641bf15489d790d0916e11c6632c7bb31641
jys2 <jys2@sfu.ca>
2018-02-14 22:16:07 -0800

    Modified SignUp page, Added attributes firstname, lastname

--- tutor_app/app/models/user.rb
+++ tutor_app/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     validates :username, presence: true, length: {maximum:50} #maximum length of the username is 50
-    validates :password, presence: true, length: {minimum:6} #minimum length of the password is 50
+    validates :password, presence: true, length: {minimum:6} #minimum length of the password is 6
+    validates :firstname, presence: true, length: {minimum:2}
+    validates :lastname, presence: true, length: {minimum:2}
 end 
