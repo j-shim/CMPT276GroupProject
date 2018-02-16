16ae38af7df3b1a6b4c6ed49ff68f54fdf37d8c0
ronnieh <ronniehung95@gmail.com>
2018-02-15 17:57:22 -0800

    Added alert message when logging out and minor change to user home page

--- tutor_app/app/controllers/sessions_controller.rb
+++ tutor_app/app/controllers/sessions_controller.rb
@@ -16,6 +16,7 @@ class SessionsController < ApplicationController
 
   def destroy
     log_out
+    flash[:success] = "You are now logged out."
     redirect_to root_url
   end
 
--- tutor_app/app/helpers/sessions_helper.rb
+++ tutor_app/app/helpers/sessions_helper.rb
@@ -4,7 +4,8 @@ module SessionsHelper
     end
 
     def current_user
-        @current_user ||= User.find_by(id: session[:user_id])
+      #  @current_user ||= User.find_by(id: session[:user_id])
+      @current_user = @current_user || User.find_by(id: session[:user_id])
     end
 
     #return true if the user is logged in, false otherwise.
