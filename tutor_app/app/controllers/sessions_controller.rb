762ae2405596f404f02372600847fce60f5222fc
ronnieh <ronniehung95@gmail.com>
2018-02-15 16:31:43 -0800

    Added alert message for log in failure and success

--- tutor_app/app/controllers/sessions_controller.rb
+++ tutor_app/app/controllers/sessions_controller.rb
@@ -5,10 +5,12 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(username:user_params[:username].downcase)
     if user && (user_params[:password]==user.password)
+      flash[:success] = "You are now logged in."
       log_in user
       redirect_to user
     else
-      render 'new'
+      flash[:danger] = 'Invalid email/password combination' # Not quite right!
+      render 'welcome/index'
     end
   end
 
