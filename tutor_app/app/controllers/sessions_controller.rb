699b1b6c823adcfecb0527f33026047a63886994
ronnieh <ronniehung95@gmail.com>
2018-02-25 16:24:54 -0800

    Testing bcrypt

--- tutor_app/app/controllers/sessions_controller.rb
+++ tutor_app/app/controllers/sessions_controller.rb
@@ -6,6 +6,9 @@ class SessionsController < ApplicationController
     user = User.find_by(username:user_params[:username].downcase)
     if user && (user_params[:password]==user.password)
       flash[:success] = "You are now logged in."
+      print "hello world \n"
+      @password = BCrypt::Password.create(user.password)
+      print @password + "\n"
       log_in user
       redirect_to user
     else
--- tutor_app/app/controllers/users_controller.rb
+++ tutor_app/app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      print "hello world"
+
       log_in @user
       redirect_to @user
     else
