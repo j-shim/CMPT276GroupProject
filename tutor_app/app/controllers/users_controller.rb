6e329398e6972252569786be40ba3a2b7a751451
zianj <zianj@sfu.ca>
2018-03-01 01:22:57 -0800

    -

--- tutor_app/app/controllers/users_controller.rb
+++ tutor_app/app/controllers/users_controller.rb
@@ -2,13 +2,11 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-
   def show
     @user = User.find(params[:id])
-    @tutor_session = current_user.tutor_sessions.build if logged_in?    
+    @tutor_session = current_user.tutor_sessions.build if logged_in?
     #debugger #used to get debugging information
   end
-
   def create
     @user = User.new(user_params)
     if @user.save
@@ -18,7 +16,18 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
   private
   def user_params
     params.require(:user).permit(:firstname,:lastname,:username,:password,:gender,:location)
