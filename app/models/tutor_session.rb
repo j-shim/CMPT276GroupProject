da0943db5381d109ec0523d8772a16bf1f3d6b89
ronnieh <ronnieh@sfu.ca>
2018-04-03 18:36:43 -0700

    Validation for start and end date for tutor sessions

--- app/models/tutor_session.rb
+++ app/models/tutor_session.rb
@@ -8,9 +8,16 @@ class TutorSession < ApplicationRecord
   validates :date, presence: true
   validates :starttime, presence: true
   validates :endtime, presence: true
+  validate :endtime_after_start_time
   before_save :calculate_time_duration
 
   def calculate_time_duration
   	self.time_duration = (self.endtime - self.starttime)/3600
+  end
+
+  def endtime_after_start_time
+    if endtime < starttime
+      errors.add(:endtime, "End time can't be before start time!")
+    end
   end
 end
